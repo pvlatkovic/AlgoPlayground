3df54ed66178e52c253a0710dce34ea5726086b0
pvlatkovic <predrag.vlatkovic@enjoying.rs>
2019-12-09 22:35:27 +0100

    proj settings and base62 basic

---
+++ Base62Hash/Base62Hash.cs
@@ -0,0 +1,23 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int test = int.MaxValue;
+		Console.WriteLine($"{test} -> {GetBase62(test)}");
+	}
+	
+	private static string base62Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
+	private static int numberOf62Characters = 62;
+	public static string GetBase62(int i)
+	{
+		var hash = "";
+		while (i > 0)
+		{
+			hash = base62Chars[(int)(i % numberOf62Characters)] + hash;
+			i /= numberOf62Characters;
+		}
+		return hash;
+	}
+}
