5998d3a9c848986ee3b57f0f6722e40e60acaf1b
Predrag Vlatkovic <prvl1605@enjoying.rs>
2019-12-10 17:40:28 +0100

    KToLast solution added

--- LinkedLists/ReturnKthToLast/solution.cs
+++ LinkedLists/ReturnKthToLast/solution.cs
@@ -4,6 +4,17 @@ namespace org.pv.AlgoPlayground.LinkedLists.ReturnKthToLast
 {
 	public class Solution
 	{
-		
+		public static Node<int> KToLast(int k, Node<int> linkedList)
+		{
+			var node = linkedList;
+			var index = 1;
+			while (node != null)
+			{
+				if (k == index++)
+					return node;
+				node = node.Next;
+			}
+			return null;
+		}
 	}
 }
