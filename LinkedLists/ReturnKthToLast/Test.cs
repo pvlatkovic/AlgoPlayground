a2c73637ec8de9ac1fc572f78286c49f7a5eac36
Predrag Vlatkovic <prvl1605@enjoying.rs>
2019-12-11 17:11:10 +0100

    KToLast added recursive version + tests, some edge cases still failing

--- LinkedLists/ReturnKthToLast/Test.cs
+++ LinkedLists/ReturnKthToLast/Test.cs
@@ -6,21 +6,39 @@ namespace org.pv.AlgoPlayground.LinkedLists.ReturnKthToLast
 {
 	public class Test
 	{
-		[Fact]
-		public void TestReturnKthToLast()
+		[Theory]
+		[InlineData(5, 9)]
+		[InlineData(0, 1)]
+		[InlineData(10, 4)]
+		public void TestReturnKthToLast(int k, int expected)
 		{
 			//Given 
 			int[] testLinkedListValues = new int[11] { 4, 4, 6, 4, 1, 9, 1, 10, 5, 11, 1 };
 			var testLinkedList = Node<int>.CreateLinkedList(testLinkedListValues);
 
 			//When 
-			var KthToLast = Solution.KToLast(5, testLinkedList);
+			var KthToLast = Solution.KToLast(k, testLinkedList);
 
-			//Then - 9 should be returned
-			Assert.Equal(KthToLast, 9);
+			//Then 
+			Assert.Equal(expected, KthToLast);
 		}
 
-		private static ArgumentException argumentException = new ArgumentException();
+		[Theory]
+		[InlineData(5, 9)]
+		[InlineData(0, 1)]
+		[InlineData(10, 4)]
+		public void TestReturnKthToLastRecursive(int k, int expected)
+		{
+			//Given 
+			int[] testLinkedListValues = new int[11] { 4, 4, 6, 4, 1, 9, 1, 10, 5, 11, 1 };
+			var testLinkedList = Node<int>.CreateLinkedList(testLinkedListValues);
+
+			//When 
+			var KthToLast = Solution.KToLastRecursive(k, testLinkedList);
+
+			//Then 
+			Assert.Equal(expected, KthToLast);
+		}
 
 		[Fact]
 		public void TestReturnKthToLastArgumentException()
@@ -58,6 +76,52 @@ namespace org.pv.AlgoPlayground.LinkedLists.ReturnKthToLast
 			try
 			{
 				var KthToLast = Solution.KToLast(200, testLinkedList);
+			}
+			// Then - ArgumentException should be thrown
+			catch (IndexOutOfRangeException)
+			{
+				isIndexOutOfRangeException = true;
+			}
+
+			Assert.True(isIndexOutOfRangeException);
+		}
+
+		[Fact]
+		public void TestReturnKthToLastRecursiveArgumentException()
+		{
+
+			//Given 
+			int[] testLinkedListValues = new int[11] { 4, 4, 6, 4, 1, 9, 1, 10, 5, 11, 1 };
+			var testLinkedList = Node<int>.CreateLinkedList(testLinkedListValues);
+
+			//When 
+			var isArgumentExceptionThrown = false;
+			try
+			{
+				var KthToLast = Solution.KToLastRecursive(-1, testLinkedList);
+			}
+			// Then - ArgumentException should be thrown
+			catch (ArgumentException)
+			{
+				isArgumentExceptionThrown = true;
+			}
+
+			Assert.True(isArgumentExceptionThrown);
+		}
+
+		[Fact]
+		public void TestReturnKthToLastRecursiveIndexOutOfRangeException()
+		{
+
+			//Given 
+			int[] testLinkedListValues = new int[11] { 4, 4, 6, 4, 1, 9, 1, 10, 5, 11, 1 };
+			var testLinkedList = Node<int>.CreateLinkedList(testLinkedListValues);
+
+			//When 
+			var isIndexOutOfRangeException = false;
+			try
+			{
+				var KthToLast = Solution.KToLastRecursive(200, testLinkedList);
 			}
 			// Then - ArgumentException should be thrown
 			catch (IndexOutOfRangeException)
--- LinkedLists/ReturnKthToLast/solution.cs
+++ LinkedLists/ReturnKthToLast/solution.cs
@@ -7,10 +7,16 @@ namespace org.pv.AlgoPlayground.LinkedLists.ReturnKthToLast
 {
 	public class Solution
 	{
+		// O(n) solution, reverse list and read first k elements
+		// space is duplicated for value types
+		// new linked list hold references therefor space is added for reference types
 		public static int KToLast(int k, Node<int> linkedList)
 		{
 			if(k < 0)
 				throw new ArgumentException("element index K cannot be less than 0");
+
+			if (linkedList == null)
+				throw new ArgumentException("Linked list cannot be null.");
 
 			var currentNode = linkedList;
 			var reversedLinkedList = new Node<int>(currentNode.Value);
@@ -30,10 +36,40 @@ namespace org.pv.AlgoPlayground.LinkedLists.ReturnKthToLast
 				if(reversedLinkedList == null)
 					throw new IndexOutOfRangeException("K is larger then number of elements in linked list");
 				reversedLinkedList = reversedLinkedList.Next;
-
 			}
 
 			return reversedLinkedList.Value;
+		}
+
+		// O(n) again,done just for fun :) O(n) space added due to recursive call stacks 
+		public static int KToLastRecursive(int k, Node<int> linkedList)
+		{
+			if (k < 0)
+				throw new ArgumentException("element index K cannot be less than 0");
+
+			if(linkedList == null)
+				throw new ArgumentException("Linked list cannot be null.");
+				
+			var ret = CheckNull(linkedList, k, 0);
+			if (ret != null)
+				return ret.Value;
+
+			throw new IndexOutOfRangeException("K is larger then number of elements in linked list");
+		}
+
+		private static Node<int> CheckNull(Node<int> linkedList, int k, int currentPosition)
+		{
+			Node<int> ret = null;
+			if(linkedList.Next != null)
+				ret = CheckNull(linkedList.Next, k, currentPosition+1);
+
+			if(ret != null)
+				return ret;
+
+			if(k == currentPosition)
+				return linkedList;
+			else 
+				return null;
 		}
 	}
 }
