6860b0760a756399c86979e0a39f3b9166dae5aa
Predrag Vlatkovic <prvl1605@enjoying.rs>
2019-12-18 16:09:54 +0100

    LoopDetection initial test code

---
+++ LinkedLists/cci2_8_LoopDetection/Solution.cs
@@ -0,0 +1,14 @@
+// Cracking coding interview 2.8 
+// given a circular linked list, find the node at the beginning of the loop
+// input 1->2->3->4->5->6->3 returns 3
+
+namespace org.pv.AlgoPlayground.LinkedLists.LoopDetection
+{
+	public class Solution
+	{
+		 public static Node<int> DetectLoop(Node<int> linkedList)
+		 {
+			 return null;
+		 }
+	}
+}
---
+++ LinkedLists/cci2_8_LoopDetection/Test.cs
@@ -0,0 +1,39 @@
+using Xunit;
+
+namespace org.pv.AlgoPlayground.LinkedLists.LoopDetection
+{
+	public class Test
+	{
+		[Fact]
+		public void TestLoopDetection()
+		{
+			//Given
+			var testList = new int[6] { 1, 2, 3, 4, 5, 6 };
+			var testLinkedList = Node<int>.CreateLinkedList(testList);
+			
+			Node<int> thirdNode = testLinkedList;
+			for (int i = 1; i < 3; i++)
+			{
+				thirdNode = thirdNode.Next;
+			}
+
+			// create circular reference
+			var node = testLinkedList;
+			while (node != null)
+			{
+				if(node.Next == null)
+				{
+					node.Next = thirdNode;
+					break;
+				}
+				node = node.Next;
+			}
+
+			//When
+			var ret = Solution.DetectLoop(testLinkedList);
+
+			//Then
+			Assert.True(thirdNode == ret);
+		}
+	}
+}
