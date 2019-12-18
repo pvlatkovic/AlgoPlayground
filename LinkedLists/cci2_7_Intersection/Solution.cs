8c9c2ca47e8d4c952c4b7a51863dc77e847a66fd
Predrag Vlatkovic <prvl1605@enjoying.rs>
2019-12-18 14:28:08 +0100

    Intersection optimized to O(N) complexity

--- LinkedLists/cci2_7_Intersection/Solution.cs
+++ LinkedLists/cci2_7_Intersection/Solution.cs
@@ -5,9 +5,9 @@ namespace org.pv.AlgoPlayground.LinkedLists.Intersection
 {
 	public class Solution
 	{
+		// brute force solution O(NxM), if n~m => O(N^2)
 		public static Node<int> GetIntersectionNodeBrute(Node<int> firstList, Node<int> secondList)
 		{
-			// brute force solution O(NxM), if n~m => O(N^2)
 			 var rootSecondList = secondList;
 			 var a = firstList;
 			 while (a != null)
@@ -23,6 +23,69 @@ namespace org.pv.AlgoPlayground.LinkedLists.Intersection
 				 a = a.Next;
 			 }
 			 return null;
+		}
+
+		// O(N) solution
+		public static Node<int> GetIntersectionNode(Node<int> firstList, Node<int> secondList)
+		{
+			var firstListLenght = GetLength(firstList);
+			var secondListLenght = GetLength(secondList);
+
+			Node<int> shorter, longer;
+			int shorterLength, longerLength;
+
+			if(firstListLenght > secondListLenght)
+			{
+				longer = firstList;
+				shorter = secondList;
+				longerLength = firstListLenght;
+				shorterLength = secondListLenght;
+			}
+			else 
+			{
+				longer = secondList;
+				shorter = firstList;
+				longerLength = secondListLenght;
+				shorterLength = firstListLenght;
+			}
+
+			var skip = longerLength - shorterLength;
+
+			// skip longer part
+			var nodeL = longer;
+			for(int i=0; i<skip; i++)
+			{
+				nodeL = nodeL.Next;
+			}
+
+			// move together until the end of the lists
+			var nodeS = shorter;
+			while(nodeL != null)
+			{
+				if(nodeL == nodeS)
+					return nodeL;
+				
+				nodeL = nodeL.Next;
+				nodeS = nodeS.Next;
+			}
+
+			return null;
+		}
+
+		private static int GetLength(Node<int> linkedList)
+		{
+			var counter = 0;
+			if(linkedList != null)
+			{
+				var node = linkedList;
+				while(node != null)
+				{
+					counter++;
+					node = node.Next;
+				}
+				return counter;
+			}
+			return 0;
 		}
 	}
 }
--- LinkedLists/cci2_7_Intersection/Test.cs
+++ LinkedLists/cci2_7_Intersection/Test.cs
@@ -5,7 +5,7 @@ namespace org.pv.AlgoPlayground.LinkedLists.Intersection
 	public class Test
 	{
 		[Fact]
-		public void TestIntersection()
+		public void TestIntersectionBrute()
 		{
 			//Given two lists which intersect (share one node)
 			// a: 1->2->3->4->5->6->null
@@ -32,6 +32,41 @@ namespace org.pv.AlgoPlayground.LinkedLists.Intersection
 
 			//When 
 			var interrsectionNode = Solution.GetIntersectionNodeBrute(testLinkedList1, testLinkedList2);
+
+			//Then
+			Assert.True(interrsectionNode != null);
+
+		}
+
+		[Fact]
+		public void TestIntersection()
+		{
+			//Given two lists which intersect (share one node)
+			// a: 1->2->3->4->5->6->null
+			// b: 7->8->^
+			var testList = new int[6] { 1, 2, 3, 4, 5, 6 };
+			var testLinkedList1 = Node<int>.CreateLinkedList(testList);
+
+			// get 3rd node
+			Node<int> thirdNode = testLinkedList1;
+			for (int i = 1; i < 3; i++)
+			{
+				thirdNode = thirdNode.Next;
+			}
+			var testList2 = new int[3] { 9, 8, 7 };
+			var testLinkedList2 = Node<int>.CreateLinkedList(testList2);
+			var node = testLinkedList2;
+			Node<int> previous = null;
+			while (node != null)
+			{
+				previous = node;
+				node = node.Next;
+			}
+			//attach third node to second list
+			previous.Next = thirdNode;
+
+			//When 
+			var interrsectionNode = Solution.GetIntersectionNode(testLinkedList1, testLinkedList2);
 
 			//Then
 			Assert.True(interrsectionNode != null);
