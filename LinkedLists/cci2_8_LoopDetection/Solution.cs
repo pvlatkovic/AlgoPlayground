1a2f0a7c12e6a4d86a15e91a7b7929dd8fed2b1a
Predrag Vlatkovic <prvl1605@enjoying.rs>
2019-12-23 16:39:23 +0100

    loop detection half way

--- LinkedLists/cci2_8_LoopDetection/Solution.cs
+++ LinkedLists/cci2_8_LoopDetection/Solution.cs
@@ -32,24 +32,42 @@ namespace org.pv.AlgoPlayground.LinkedLists.LoopDetection
 
 		public static Node<int> DetectLoopRacingPointers(Node<int> linkedList)
 		{
-			//TODO: add implementation
 			// having two pointers moving simultaniously at different speeds. Faster will outrun slower eventualy...
 			var nodeSlow = linkedList;
 			var nodeFast = linkedList;
+
+			int nodeSlowCount = 0, nodeFastCount = 0;
+
 			while (nodeSlow != null)
 			{
+				nodeSlowCount++;
 				for(int i=0; i < 2; i++)
 				{
-					var prev = nodeFast;
+					nodeFastCount++;
 					nodeFast = nodeFast.Next;
 					if(nodeFast == null)
 						return null;
 					if(nodeSlow == nodeFast)
 					{
-						return prev;
+						// now count number of elements in the circle (e.g. 1-2-3-4-1 is a circle)
+						var startCircleNode = nodeSlow; 
+						var node = nodeSlow.Next;
+
+						var circleCount = 1;
+
+						while(node != startCircleNode)
+						{
+							circleCount++;
+							node = node.Next;
+						}
+						//now what? :)
+						// we have number of "slow" nodes until collision 
+						// as well as number of "fast" nodes and size of "circle"
+
+
 					}
 				}
-				
+
 				nodeSlow = nodeSlow.Next;				
 			}
 			
