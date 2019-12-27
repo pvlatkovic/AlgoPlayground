8ef358b224afa0c0059933b2836706247bfcc883
pvlatkovic <predrag.vlatkovic@enjoying.rs>
2019-12-27 19:17:10 +0100

    two strings are permutations basic solution and test

--- ArraysAndStrings/cci1_2_TwoStringsArePerutations/Solution.cs
+++ ArraysAndStrings/cci1_2_TwoStringsArePerutations/Solution.cs
@@ -2,12 +2,26 @@ namespace org.pv.AlgoPlayground.ArraysAndStrings.TwoStringsArePermutations
 {
 	public class Solution
 	{
+		// O(N)
 		public static bool CheckTwoStringsArePermutations(string s1, string s2) 
 		{
 			if (s1.Length != s2.Length)
 				return false;
 
-			//TODO: implement this
+			var charCountS1 = new int[128];
+			var charCountS2 = new int[128];
+
+			for(int i = 0; i < s1.Length; i++)
+			{
+				charCountS1[s1[i]]++;
+				charCountS2[s2[i]]++;
+			}
+
+			for(int i=0; i < charCountS1.Length; i++)
+			{
+				if(charCountS1[i] != charCountS2[i])
+					return false;
+			}
 
 			return true;
 		}
--- ArraysAndStrings/cci1_2_TwoStringsArePerutations/Test.cs
+++ ArraysAndStrings/cci1_2_TwoStringsArePerutations/Test.cs
@@ -4,18 +4,19 @@ namespace org.pv.AlgoPlayground.ArraysAndStrings.TwoStringsArePermutations
 {
 	public class Test
 	{
-		[Fact]
-		public void TestIfTwoStringsArePermutations()
+		[Theory]
+		[InlineData("bulevar", "levubar", true)]
+		[InlineData("bulevar", "levubac", false)]
+		[InlineData("aaaaaaaaa", "aaaaaaaaa", true)]
+		public void TestIfTwoStringsArePermutations(string s1, string s2, bool resultExpected)
 		{
-			//Given
-			var s1 = "bulevar";
-			var s2 = "levubar";
+			//Given s1 and s2
 
 			//When
 			var result = Solution.CheckTwoStringsArePermutations(s1, s2);
 
 			//Then
-			Assert.True(result);
+			Assert.Equal(resultExpected, result);
 		}
 	}
 }
