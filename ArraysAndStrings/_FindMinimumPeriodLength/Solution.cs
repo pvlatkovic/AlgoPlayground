2945f6194dc5afba85e96ee70382c13bafbaac57
Predrag Vlatkovic <prvl1605@enjoying.rs>
2020-01-04 13:09:13 +0100

    Find minimum period length of an array

---
+++ ArraysAndStrings/_FindMinimumPeriodLength/Solution.cs
@@ -0,0 +1,16 @@
+/*
+You are given an array a of length n = 2m. Write a function that finds length of the minimal period T of array a.
+
+int MinPeriod(int * a, int n)
+
+Number T is a period of array a, if repeating first T elements of array a several times gives exactly array a, without any extra elements.
+
+Examples:
+
+Input	Output
+n = 8		4
+a = {2, 5, 3, 4, 2, 5, 3, 4}		
+n = 8		8
+a = {2, 5, 3, 2, 5, 3, 2, 5}
+
+*/
