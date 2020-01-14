9d24e54ed8205375ec895f7a90f0c6102dfc3658
Predrag Vlatkovic <prvl1605@enjoying.rs>
2020-01-14 12:03:46 +0100

    1 or 0 edits away solution + tests

---
+++ ArraysAndStrings/cci1_5_EditsAway/Solution.cs
@@ -0,0 +1,38 @@
+/*
+	Cracking the coding interview - interview questions 1.5
+	Edit operations (edits): insert, remove and replaced character. 
+	Given twop strings check if they are one edit (or zero edits) away.
+*/
+
+namespace org.pv.AlgoPlayground.ArraysAndStrings.EditsAway
+{
+	public class Solution
+	{
+		public static bool IsOneOrZeroEditsAway(string s, string t)
+		{
+			if(System.Math.Abs(s.Length - t.Length) > 1)
+				return true;
+
+			var cs = new int[128]; //ascii again
+			var ct = new int[128];
+
+			for(int i = 0; i < s.Length; i++)
+				cs[s[i]] += 1;
+
+			for(int i = 0; i < t.Length; i++)
+				ct[t[i]] += 1;
+
+			var x = 0;
+			for(int i = 0; i < 128; i++)
+			{
+				cs[i] -= ct[i];
+				x += System.Math.Abs(cs[i]);
+			}
+
+			if(x > 1)
+				return false;
+
+			return true;
+		}
+	}
+}
---
+++ ArraysAndStrings/cci1_5_EditsAway/Test.cs
@@ -0,0 +1,23 @@
+using Xunit;
+
+namespace org.pv.AlgoPlayground.ArraysAndStrings.EditsAway
+{
+	public class Test
+	{
+		[Theory]
+		// [InlineData("pale", "ple", true)]
+		// [InlineData("pales", "pale", true)]
+		// [InlineData("pale", "bale", true)]
+		[InlineData("pale", "bake", false)]
+		public void OneOrZeroEditsAway(string a, string b, bool resultExpected)
+		{
+			//Given input
+
+			//When
+			var result = Solution.IsOneOrZeroEditsAway(a, b);
+
+			//Then
+			Assert.Equal(resultExpected, result);
+		}
+	}
+}
