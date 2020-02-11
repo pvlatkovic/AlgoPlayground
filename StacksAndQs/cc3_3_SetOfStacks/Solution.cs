4ea6501a053479c4f74c9fbafe66dc6f2d6f7287
pvlatkovic <predrag.vlatkovic@enjoying.rs>
2020-02-11 22:55:52 +0100

    started set of stacks

---
+++ StacksAndQs/cc3_3_SetOfStacks/Solution.cs
@@ -0,0 +1,43 @@
+// Cracking the coding interview - interview questions 3.3
+// SetOfStacks composed of several stacks, create new stack when previous one exceedes capacity
+// test capacity is 10 stacks per 5 elements
+
+using System;
+
+namespace org.pv.AlgoPlayground.StacksAndQs.SetOfStacks
+{
+
+	// data structure definition
+	public class SetOfStack<T>
+	{
+		/*
+		private Node<Stack<T>> stackOfStacks;
+		private Node<T> currentStack;
+
+		private int currentStackIndex = 0;
+		private int currentElementIndex = 0;
+		private int maxElementsPerStack = 5;
+		private int maxStacks = 10;
+
+		public void Push(T value)
+		{
+			if(stackOfStacks == null)
+			{
+				var stack = new Stack<T>();
+				stack.Push(value);
+				currentElementIndex++;
+				
+				stackOfStacks = new Node<Stack<T>>(stack);
+			}
+			else
+			{
+				if(currentElementIndex == 6)
+
+			}
+
+
+			
+		}
+		*/
+	}
+}
