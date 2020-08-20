0572b3344d6f3d33a57ff53e62fa09e72d11df1c
Predrag Vlatkovic <predrag.vlatkovic@enjoying.rs>
2020-08-20 10:29:06 +0200

    start - counter clockwise traversal of a binary tree

---
+++ _MSP/CounterClockWiseBinTree/Solution.cs
@@ -0,0 +1,22 @@
+/*
+print out the counter clockwise traversal of a binary tree
+	
+Input:
+     1
+   /  \
+  2    3
+ / \  / \
+4   5 6  7 
+Output: 1 4 5 6 7 3 2
+
+Input:
+      1
+     /  \
+    2    3
+   /    / \
+  4    5   6
+ / \  /   / \
+7   8 9  10  11
+Output: 1 7 8 9 10 11 3 2 4 5 6
+
+*/
