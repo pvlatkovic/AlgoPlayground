a2b579559a5b62cb60816b53785032737fdd8189
Predrag Vlatkovic <predrag.vlatkovic@enjoying.rs>
2020-08-21 17:35:04 +0200

    Difference 2 longest related nodes in bin tree

--- _MSP/DiffTwoLongestRelatedNodesInBinTree/Solution.cs
+++ _MSP/DiffTwoLongestRelatedNodesInBinTree/Solution.cs
@@ -6,39 +6,78 @@
 //     4     5 
 //      \   / \  
 //       8 6   7 
-//      / 
-//     9 
-// longest distance between two nodes is [9, 8, 4, 2, 5, 7], difference between two end nodes is 9 - 7 = 2
+//      /       \
+//     9        10
+// longest distance between two nodes is [9, 8, 4, 2, 5, 7, 10], difference between two end nodes is |9 - 10| = 1
 
 using System;
+using System.Diagnostics;
 using org.pv.TreesAndGraphs.Common;
 
 namespace org.pv.AlgoPlayground.MSP.DiffTwoLongestRelatedNodesInBinTree
 {
-	public class Solution 
+	public class Solution
 	{
 		public static int Execute(NodeBinTree<int> tree)
 		{
 			// find max left height + max right height + 1
-			var max = int.MinValue;
-			var maxH = MaxHeight(tree, ref max);
+			var max = 0;
+			var maxDiametarNode = tree;
+			Diametar(tree, ref max, ref maxDiametarNode);
 
-			return max;
+			var leftH = Height(maxDiametarNode.Left);
+			var rightH = Height(maxDiametarNode.Right);
+
+			int a = int.MinValue;
+			int b = int.MinValue;
+
+			GetNthElement(maxDiametarNode.Left, leftH - 1, ref a);
+			GetNthElement(maxDiametarNode.Right, rightH - 1, ref b);
+
+			return Math.Abs(a - b);
 		}
 
-		private static int MaxHeight(NodeBinTree<int> root, ref int max)
+		private static void GetNthElement(NodeBinTree<int> node, int level, ref int nth)
 		{
-			if(root == null)
+			if (node == null)
+				return;
+			if (level == 0)
+				nth = node.Value;
+			else
+			{
+				GetNthElement(node.Left, level - 1, ref nth);
+				GetNthElement(node.Right, level - 1, ref nth);
+			}
+		}
+
+		private static void Diametar(NodeBinTree<int> node, ref int maxd, ref NodeBinTree<int> maxDiametarNode)
+		{
+			if (node == null)
+				return;
+
+			var d = 1 + Height(node.Left) + Height(node.Right);
+
+			if (d > maxd)
+			{
+				maxd = d;
+				maxDiametarNode = node;
+			}
+
+			Diametar(node.Left, ref maxd, ref maxDiametarNode);
+			Diametar(node.Right, ref maxd, ref maxDiametarNode);
+		}
+
+		private static int Height(NodeBinTree<int> node)
+		{
+			if (node == null)
 				return 0;
 
-			var maxL = MaxHeight(root.Left, ref max);
-			var maxR = MaxHeight(root.Right, ref max);
+			var leftHeight = Height(node.Left);
+			var rightHeigt = Height(node.Right);
 
-			var maxC = 1 + maxL + maxR; 
-
-			max = Math.Max(maxC, max);
-
-			return maxC;
+			return 1 + Math.Max(leftHeight, rightHeigt);
 		}
+
+
 	}
 }
--- _MSP/DiffTwoLongestRelatedNodesInBinTree/Test.cs
+++ _MSP/DiffTwoLongestRelatedNodesInBinTree/Test.cs
@@ -14,13 +14,33 @@ using Xunit;
 
 namespace org.pv.AlgoPlayground.MSP.DiffTwoLongestRelatedNodesInBinTree
 {
-	
+
 	public class Test
 	{
 		[Fact]
 		public void TestDiffTwoLongestRelatedNodesInBinTree()
 		{
+			// arrange
+			var tree = new NodeBinTree<int>(1);
 
+			tree.Left = new NodeBinTree<int>(2);
+			tree.Right = new NodeBinTree<int>(3);
+
+			tree.Left.Left = new NodeBinTree<int>(4);
+			tree.Left.Right = new NodeBinTree<int>(5);
+
+			tree.Left.Left.Right = new NodeBinTree<int>(8);
+			tree.Left.Right.Left = new NodeBinTree<int>(6);
+			tree.Left.Right.Right = new NodeBinTree<int>(7);
+			tree.Left.Right.Right.Right = new NodeBinTree<int>(10);
+
+			tree.Left.Left.Right.Left = new NodeBinTree<int>(9);
+
+			// act
+			var result = Solution.Execute(tree);
+
+			// assert
+			Assert.Equal(1, result);
 		}
 	}
 }
